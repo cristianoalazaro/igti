cf0368741d254111a719ac6f04aab4cb5cfc5cee
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2020-08-06 06:44:57 -0300

    '060820a'

--- node/my-bank-api/index.js
+++ node/my-bank-api/index.js
@@ -3,6 +3,8 @@ import winston from 'winston';
 import accountsRouter from './routes/account.js';
 import { promises as fs } from 'fs';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocument } from './doc.js';
 
 const { readFile, writeFile } = fs;
 
@@ -10,8 +12,8 @@ global.fileName = 'accounts.json';
 
 const { combine, timestamp, label, printf } = winston.format;
 
-const myFormat = printf(({ level, messsage, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${messsage}`;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
 });
 
 //prettier-ignore
@@ -30,9 +32,10 @@ global.logger = winston.createLogger({
 
 const app = express();
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 app.use(express.static('public'));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/account', accountsRouter);
 
 app.listen(3000, async () => {
