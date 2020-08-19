0479396a34a9612c9dcd6e9df6316ba893b1e64a
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2020-08-19 19:52:41 -0300

    '190820b'

--- React/aulas/aula-12/react-desafio-04/frontend/src/App.js
+++ React/aulas/aula-12/react-desafio-04/frontend/src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import * as api from './api/apiService';
+import spinner from './components/Spinner';
+import Spinner from './components/Spinner';
+import GradesControl from './components/GradesControl';
 
 export default function App() {
-  const testAPI = async () => {
-    const result = await api.getAllGrades();
-    console.log(result);
+  const [allGrades, setAllGrades] = useState([]);
+  const [selectedGrade, setSelectedGrade] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const getGrades = async () => {
+      const grades = await api.getAllGrades();
+
+      setTimeout(() => {
+        setAllGrades(grades);
+      }, 2000);
+    };
+    // api.getAllGrades().then((grades) => {
+    //   setTimeout(() => {
+    //     setAllGrades(grades);
+    //   }, 2000);
+    // });
+    getGrades();
+  }, []);
+
+  const handleDelete = () => {
+    console.log('handleDelete');
   };
-  testAPI();
-  return <h1>Olá, tudo bem</h1>;
+
+  const handlePersist = () => {
+    console.log('handlePersist');
+  };
+
+  return (
+    <div>
+      <h1 className="center">Controle de Notas</h1>
+      {allGrades.length > 0 && (
+        <GradesControl
+          grades={allGrades}
+          onDelete={handleDelete}
+          onPersist={handlePersist}
+        />
+      )}
+      {allGrades.length == 0 && <Spinner />}
+    </div>
+  );
 }
