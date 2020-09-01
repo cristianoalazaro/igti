b0ba81d16ab89408cb4832eaa97ef32f3bb5f81d
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2020-09-01 06:32:10 -0300

    010920a

--- mongodb/My-bank-app/app.js
+++ mongodb/My-bank-app/app.js
@@ -3,10 +3,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { accountRouter } from './routes/accountRouter.js';
 
+require('dotenv').config();
+
 //Conexão com o mongoDB
 mongoose
   .connect(
-    'mongodb+srv://cristianoalazaro:clazaro@cluster0.f1162.azure.mongodb.net/my-bank?retryWrites=true&w=majority',
+    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPWD}'@cluster0.f1162.azure.mongodb.net/my-bank?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -19,12 +21,10 @@ mongoose
     console.log('Erro ao conectar com o mongoDB: ' + err);
   });
 
-app.put('/student/:id', async(((req, res) = {})));
-
 const app = express();
 app.use(express.json());
 app.use(accountRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('API iniciada!');
 });
